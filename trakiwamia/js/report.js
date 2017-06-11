55a0cbbb4c4b7477e6b3b17eea0a5f5d1a53e6ea
schwarz <schwarz@localhost.localdomain>
2017-06-11 17:05:07 +0300

    trakiwamiaaaa

---
+++ trakiwamia/js/report.js
@@ -0,0 +1,103 @@
+function calc_total(){
+    var sum = 0;
+    $('.input-amount').each(function(){
+        sum += parseFloat($(this).text());
+    });
+    $(".preview-total").text(sum);
+}
+$(document).on('click', '.input-remove-row', function(){
+    var tr = $(this).closest('tr');
+    tr.fadeOut(200, function(){
+    	tr.remove();
+	   	calc_total()
+	});
+});
+
+$(function(){
+    $('.preview-add-button').click(function(){
+        var form_data = {};
+        form_data["concept"] = 'Water';
+        form_data["description"] = 40000;
+        form_data["amount"] =$('.payment-form input[name="date"]').val();
+        form_data["status"] =  40000-$('.payment-form input[name="date"]').val();
+        form_data["date"] =$('.payment-form #status option:selected').text();
+        form_data["remove-row"] = '<span class="glyphicon glyphicon-remove"></span>';
+        var row = $('<tr></tr>');
+        $.each(form_data, function( type, value ) {
+            $('<td class="dark" class="input-'+type+'"></td>').html(value).appendTo(row);
+        });
+        $('.preview-table > tbody:last').append(row);
+        calc_total();
+    });
+});
+
+
+$(document).ready(function() {
+
+    var navListItems = $('ul.setup-panel li a'),
+        allWells = $('.setup-content');
+
+    allWells.hide();
+
+    navListItems.click(function(e)
+    {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+            $item = $(this).closest('li');
+
+        if (!$item.hasClass('disabled')) {
+            navListItems.closest('li').removeClass('mactive');
+            navListItems.closest('li').addClass('mamamia');
+            $item.addClass('mactive');
+            allWells.hide();
+            $target.show();
+        }
+    });
+
+    $('ul.setup-panel li.mactive a').trigger('click');
+
+    // DEMO ONLY //
+    $('#activate-step-2').on('click', function(e) {
+        $('ul.setup-panel li:eq(1)').removeClass('disabled');
+        $('ul.setup-panel li a[href="#step-2"]').trigger('click');
+        $(this).remove();
+    })
+
+    $('#activate-step-3').on('click', function(e) {
+        $('ul.setup-panel li:eq(2)').removeClass('disabled');
+        $('ul.setup-panel li a[href="#step-3"]').trigger('click');
+        $(this).remove();
+    })
+
+    $('#activate-step-4').on('click', function(e) {
+        $('ul.setup-panel li:eq(3)').removeClass('disabled');
+        $('ul.setup-panel li a[href="#step-4"]').trigger('click');
+        $(this).remove();
+    })
+
+    $('#activate-step-3').on('click', function(e) {
+        $('ul.setup-panel li:eq(2)').removeClass('disabled');
+        $('ul.setup-panel li a[href="#step-3"]').trigger('click');
+        $(this).remove();
+    })
+});
+
+
+// Add , Dlelete row dynamically
+
+     $(document).ready(function(){
+      var i=1;
+     $("#add_row").click(function(){
+      $('#addr'+i).html("<td>"+ (i+1) +"</td><td><input name='name"+i+"' type='text' placeholder='Name' class='form-control input-md'  /> </td><td><input  name='sur"+i+"' type='text' placeholder='Surname'  class='form-control input-md'></td><td><input  name='email"+i+"' type='text' placeholder='Email'  class='form-control input-md'></td><td><select type='text' name='gender"+i+"' class='form-control'><option name='male"+i+"' value='male'>Male</option><option name='Female"+i+"' value='Female'>Female</option><option name='3rdgen"+i+"' value='none'>None</option></select></td>");
+
+      $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+      i++;
+  });
+     $("#delete_row").click(function(){
+    	 if(i>1){
+		 $("#addr"+(i-1)).html('');
+		 i--;
+		 }
+	 });
+
+});
